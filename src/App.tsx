d45dd0209043c45fc5d119d049e30aa9b8e46d61
Johannes Andersson <hello@thejoltjoker.com>
2024-12-27 19:29:30 +0100

    feat: fingerprinting setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
+import "./App.css";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fpHash, setFpHash] = useState("");
+
+  // create and set the fingerprint as soon as
+  // the component mounts
+  useEffect(() => {
+    const setFp = async () => {
+      const fp = await FingerprintJS.load();
+
+      const { visitorId } = await fp.get();
+
+      setFpHash(visitorId);
+    };
+
+    setFp();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="crowdplay logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>This is the fingerprint hash</h1>
+        <h3>Hash: {fpHash}</h3>
       </div>
-      <h1>crowdplay</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/lib/fingerprint.ts
@@ -0,0 +1,1 @@
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
